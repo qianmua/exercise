788f84f4af0c5a237d7737411816d3cde04e2d95
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-03-03 14:36:51 +0800

    alg

--- src/pres/hjc/main/alg202001/Main.java
+++ src/pres/hjc/main/alg202001/Main.java
@@ -3,6 +3,7 @@ package pres.hjc.main.alg202001;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.TreeSet;
 
 /**
@@ -195,8 +196,81 @@ public class Main {
 
     }
 
+    /*
+     *
+     冰雹数
+
+     任意给定一个正整数N，
+     如果是偶数，执行： N / 2
+     如果是奇数，执行： N * 3 + 1
+
+     生成的新的数字再执行同样的动作，循环往复。
+
+     通过观察发现，这个数字会一会儿上升到很高，
+     一会儿又降落下来。
+     就这样起起落落的，但最终必会落到“1”
+     这有点像小冰雹粒子在冰雹云中翻滚增长的样子。
+
+     比如N=9
+     9,28,14,7,22,11,34,17,52,26,13,40,20,10,5,16,8,4,2,1
+     可以看到，N=9的时候，这个“小冰雹”最高冲到了52这个高度。
+
+     输入格式：
+     一个正整数N（N<1000000）
+     输出格式：
+     一个正整数，表示不大于N的数字，经过冰雹数变换过程中，最高冲到了多少。
+
+     例如，输入：
+     10
+     程序应该输出：
+     52
+
+     再例如，输入：
+     100
+     程序应该输出：
+     9232
+
+     资源约定：
+     峰值内存消耗（含虚拟机） < 256M
+     CPU消耗  < 1000ms
+
+
+     请严格按要求输出，不要画蛇添足地打印类似：“请您输入...” 的多余内容。
+
+     所有代码放在同一个源文件中，调试通过后，拷贝提交该源码。
+     注意：不要使用package语句。不要使用jdk1.7及以上版本的特性。
+     注意：主类的名字必须是：Main，否则按无效代码处理。
+     */
+    private static void BingBaoShu(){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+//        TreeSet set = new TreeSet();
+        int temp = 1;
+        int max = 0;
+        while ( temp++ < a){
+            int n = addSum(temp,temp);
+            if (n > max) max = n;
+        }
+        System.out.println(max);
+    }
+
+    private static int addSum(int a,int max){
+        if (a == 1) {
+            return max;
+        }else if (a % 2 == 0){
+                a = a / 2;
+                if (a > max) max = a;
+                return addSum(a,max);
+            }else{
+                a = a * 3 + 1;
+                if (a > max) max = a;
+                return addSum(a,max);
+            }
+
+    }
+
     public static void main(String[] args) {
-        couSuanShi();
+        BingBaoShu();
     }
 }
 
