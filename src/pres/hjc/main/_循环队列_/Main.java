9c463d9e55bd7d1d62aba20114ee1081cffbaf36
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-04-24 22:36:25 +0800

    alg

--- src/pres/hjc/main/_循环队列_/Main.java
+++ src/pres/hjc/main/_循环队列_/Main.java
@@ -12,7 +12,19 @@ package pres.hjc.main._循环队列_;
 public class Main {
 
     public static void main(String[] args) {
+        LoopQueue queue = new LoopQueue(20);
 
+        queue.setFront(1);
+        queue.setFront(2);
+        queue.setFront(3);
+        queue.setFront(4);
+        queue.setFront(5);
+        queue.setFront(6);
+
+        System.out.println(queue.getFront());
+        System.out.println(queue.getFront());
+        System.out.println(queue.getFront());
+        System.out.println(queue.getFront());
     }
 }
 
@@ -23,43 +35,72 @@ class LoopQueue{
     private int maxSize;
     private int queue[];
 
-    public LoopQueue(int maxSize){
+    /**
+     * init
+     * @param maxSize
+     */
+    LoopQueue(int maxSize){
         front = 0;
         rear = 0;
         this.maxSize = maxSize;
         queue = new int[this.maxSize];
     }
 
-    public boolean isEmpty(){
+    /**
+     * is empty
+     * @return
+     */
+    private boolean isEmpty(){
         return front == rear ;
     }
 
+    /**
+     * full
+     * @return
+     */
     public boolean isFull(){
         return ((rear + 1) % maxSize) == front;
     }
 
-    public void getFront(int front) {
+    /**
+     * 队头
+     */
+    public int getFront() {
         if (!isEmpty()){
-            front = queue[(this.front + 1) % maxSize];
+            int temp = front;
+            front = (this.front + 1) % maxSize;
+            return queue[temp];
         }else {
             System.out.println("Empty");
-            return;
+            return -1;
         }
     }
 
-    public void setFront(int front) {
+    /**
+     * 压队
+     * @param rear
+     */
+    public void setFront(int rear) {
         if (!isFull()){
-            rear = (rear + 1 ) % maxSize;
-            this.queue[rear] = front;
+            this.rear = (rear + 1 ) % maxSize;
+            this.queue[this.rear] = rear;
         }else {
             System.out.println("Full");
             return;
         }
     }
 
-    public void movQueue(){
+    /**
+     * 移除
+     */
+    public int movQueue(){
         if (!isEmpty()){
+            int temp = front;
             front = (front + 1) % maxSize;
+            return temp;
+        }else {
+            System.out.println("Empty");
+            return -1;
         }
     }
 }
